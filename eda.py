d13c1421070a70702e23cbdcae09d84e08e5f78a
Paul Birt <paulbirt1998@gmail.com>
2025-07-07 16:50:28 +0100

    Join train and weather data based on timestamp and station code

---
+++ eda.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('eus_liv_joined.csv')
+
+print(df.info())
--- join_data.py
+++ join_data.py
@@ -1,24 +1,29 @@
 import pandas as pd
 
-train_df = pd.read_csv("eda_data_with_delays.csv")
-weather_df = pd.read_csv("cre_eda_weather.csv")
+train_df = pd.read_csv("eus_liv_w_derived_features.csv")
+weather_df = pd.read_csv("eus_liv_weather.csv")
 
 # Ensure datetime columns are parsed correctly
-train_df['EUS Scheduled Departure Time'] = pd.to_datetime(train_df['EUS Scheduled Departure Time'])
-weather_df['date'] = pd.to_datetime(weather_df['date'], utc=True)  # force UTC timezone awareness
+train_df['Actual Time'] = pd.to_datetime(train_df['Actual Time'])
+weather_df['date'] = pd.to_datetime(weather_df['date'])
 
-# Convert train_df departure_hour to UTC to match weather_df['date']
-train_df['departure_hour'] = train_df['EUS Scheduled Departure Time'].dt.round('h').dt.tz_localize('Europe/London').dt.tz_convert('UTC')
+#round train passing to nearest hour
+train_df['Nearest Hour'] = train_df['Actual Time'].dt.round('h')
+train_df['Nearest Hour'] = train_df['Nearest Hour'].dt.tz_localize('Europe/London', 
+                                                                   nonexistent='NaT',
+                                                                   ambiguous='NaT').dt.tz_convert('UTC')
 
-# Merge with weather data on datetime
-merged_df = train_df.merge(
+#drop trains within an hour of time zone changes
+train_df = train_df.dropna(subset=['Nearest Hour'])
+
+
+# Merge with weather data on datetime 
+merged_df = pd.merge(
+    train_df,
     weather_df,
-    left_on='departure_hour',
-    right_on='date',
+    left_on=['Nearest Hour', 'Station'],
+    right_on=['date', 'Station'],
     how='left'
 )
 
-# Check result
-print(merged_df.head())
-
-merged_df.to_csv("cre_merged_eda_data.csv")
+merged_df.to_csv("eus_liv_joined.csv", index=False)
